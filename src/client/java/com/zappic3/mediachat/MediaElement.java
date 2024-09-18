38234005b82aa9b1ec01cf23e0ddeac5c390d6e2
Zappic3 <79416867+Zappic3@users.noreply.github.com>
2024-09-18 19:38:47 +0200

    + improved image downloading (Added file-type checking)

--- src/client/java/com/zappic3/mediachat/MediaElement.java
+++ src/client/java/com/zappic3/mediachat/MediaElement.java
@@ -3,13 +3,18 @@ package com.zappic3.mediachat;
 import net.minecraft.util.Identifier;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URI;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,10 +27,12 @@ import static com.zappic3.mediachat.Utility.registerTexture;
 
 public class MediaElement {
     private static final Identifier MEDIA_LOADING =  Identifier.of("media-chat", "textures/media_loading.png");
-    private static Map<String, MediaElement> _mediaPool = new ConcurrentHashMap<>();
+    private static final Identifier MEDIA_UNSUPPORTED =  Identifier.of("media-chat", "textures/media_unsupported.png");
+    private static final Identifier MEDIA_DOWNLOAD_FAILED =  Identifier.of("media-chat", "textures/media_download_failed.png");
+    private static final Map<Integer, MediaElement> _mediaPool = new ConcurrentHashMap<>();
+    private final CompletableFuture<Void> loadFuture;
     private final String _source;
     private volatile Identifier _id;
-    private final CompletableFuture<Void> loadFuture;
     private int _width;
     private int _height;
 
@@ -42,20 +49,47 @@ public class MediaElement {
     }
 
     public static MediaElement of(String source) {
-        return _mediaPool.computeIfAbsent(source, s -> new MediaElement(s, MEDIA_LOADING));
+        return _mediaPool.computeIfAbsent(source.hashCode(), s -> new MediaElement(source, MEDIA_LOADING));
     }
 
     private static MediaIdentifierInfo downloadMedia(String source) {
         try {
-            URL url = new URL(source);
-            BufferedImage image = ImageIO.read(url);
+            URL url = new URI(source).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
 
-            //String filePath = getModDataFolderPath("temp").resolve(source.hashCode() + ".png").toString();
-            //LOGGER.info(filePath);
-            //File file = new File(filePath);
-            //ImageIO.write(image, "png", file); // todo add option to save images to disk
-            Identifier id = registerTexture(image, source.hashCode()+"");
-            return new MediaIdentifierInfo(id, image.getWidth(), image.getHeight());
+            String contentType = connection.getContentType();
+            int contentLength = connection.getContentLength(); // todo implement some sort of file size limit to prevent abuse
+            if (contentType.startsWith("image") && contentLength > 0) {
+                ImageInputStream imageStream = ImageIO.createImageInputStream(url.openStream());
+
+                Iterator<ImageReader> readers = ImageIO.getImageReaders(imageStream);
+                if (!readers.hasNext()) {
+                    imageStream.close();
+                    return new MediaIdentifierInfo(MEDIA_UNSUPPORTED, 64, 64);
+                }
+
+                ImageReader reader = readers.next();
+                String formatName = reader.getFormatName();
+                reader.setInput(imageStream);
+                BufferedImage image = reader.read(0);
+                imageStream.close();
+                // todo add option to save images to disk
+
+                if (image != null && image.getWidth() > 0 && image.getHeight() > 0) {
+                    Identifier id = registerTexture(image, source.hashCode()+"");
+                    return new MediaIdentifierInfo(id, image.getWidth(), image.getHeight());
+                } else {
+                    return new MediaIdentifierInfo(MEDIA_DOWNLOAD_FAILED, 64, 64);
+                }
+
+            } else {
+                return new MediaIdentifierInfo(MEDIA_UNSUPPORTED, 64, 64);
+            }
+
+
+
         } catch (IOException e) {
             // handle IOException
             LOGGER.error("Error Downloading Image: \n"+e.getMessage());
@@ -63,6 +97,13 @@ public class MediaElement {
             LOGGER.error("Error while registering image: \n" + e.getMessage());
         }
         return new MediaIdentifierInfo(Identifier.of("media-chat", "textures/image.png"), 64, 64);
+    }
+
+    // this is intended for loading media that was saved to disk on startup.
+    public static void add(Identifier id, int hashCode, int width, int height) {
+        MediaElement e = new MediaElement(null, id);
+        e.setId(id, width, height);
+        _mediaPool.put(hashCode, e);
     }
 
     public Identifier currentFrame() {
