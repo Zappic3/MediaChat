92584b5d552bd08848daa3b5705f8eea0aaa7a36
Zappic3 <79416867+Zappic3@users.noreply.github.com>
2026-05-03 14:09:24 +0200

    removed unused imports

--- src/client/java/com/zappic3/mediachat/mixin/client/ChatScreenMixin.java
+++ src/client/java/com/zappic3/mediachat/mixin/client/ChatScreenMixin.java
@@ -1,36 +1,24 @@
 package com.zappic3.mediachat.mixin.client;
 
-import com.zappic3.mediachat.ConfigModel;
 import com.zappic3.mediachat.FileUploadHandler;
 import com.zappic3.mediachat.IMediaChatPaste;
-import com.zappic3.mediachat.filesharing.filesharing.FileSharingService;
-import com.zappic3.mediachat.ui.ConfirmUploadScreen;
 import com.zappic3.mediachat.ui.GifBrowserUI;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.net.URI;
-import java.net.URL;
+
 import java.nio.file.Path;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
-import static com.zappic3.mediachat.MediaChat.*;
-import static com.zappic3.mediachat.Utility.displayErrorMessage;
 import static net.minecraft.client.util.InputUtil.GLFW_KEY_ESCAPE;
 
 @Mixin(ChatScreen.class)
